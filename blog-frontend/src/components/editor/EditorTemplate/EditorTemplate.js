7d292fb13d569258c96290d35d2bed072bc8790f
kyle_koh <ykoh728@gmail.com>
2018-10-25 14:10:04 +0900

    add EditorTemplate.js

--- blog-frontend/src/components/editor/EditorTemplate/EditorTemplate.js
+++ blog-frontend/src/components/editor/EditorTemplate/EditorTemplate.js
@@ -16,23 +16,51 @@ class EditorTemplate extends Component {
 		});
 	}
 
+	// 마우스를 뗏을 때 등록한 이벤트 제거
 	handleMouseUp = (e) => {
 		document.body.removeEventListener('mousemove', this.handleMouseMove);
 		window.removeEventListener('mouseup', this.handleMouseUp);
 	}
 
+	// seperator 클릭할 때
+	handleSeperatorMouseDown = (e) => {
+		document.body.addEventListener('mousemove', this.handleMouseMove);
+		window.addEventListener('mouseup', this.handleMouseUp);
+	}
+
 	render() {
 		const { header, editor, preview } = this.props;
+		const { leftPercentage } = this.state;
+		const { handleSeperatorMouseDown } = this;
+
+		//각 영역에 flex 값 적용
+		const leftStyle = {
+			flex: leftPercentage
+		}
+
+		const rightStyle = {
+			flex: 1 - leftPercentage
+		}
+
+		// separator 위치 설정
+		const separatorStyle = {
+			left: `${leftPercentage * 100}%`
+		}
+
 		return (
 			<div className={cx('editor-template')}>
 				{header}
 				<div className={cx('panes')}>
-					<div className={cx('pane', 'editor')}>
+					<div className={cx('pane', 'editor')} style={leftStyle}>
 						{editor}
 					</div>
-					<div className={cx('pane', 'preview')}>
+					<div className={cx('pane', 'preview')} style={rightStyle}>
 						{preview}
 					</div>
+					<div
+						className={cx('separator')}
+						style={separatorStyle}
+						onMouseDown={handleSeperatorMouseDown}/>
 				</div>
 			</div>
 		)
