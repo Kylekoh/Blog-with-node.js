90e89667bb64b04e53e84c5be1b746035039e503
kyle_koh <ykoh728@gmail.com>
2018-10-25 15:53:56 +0900

    update EditorPane

---
+++ blog-frontend/src/components/common/MarkdownRender/MarkdownRender.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import styles from './MarkdownRender.scss';
+import classNames from 'classnames/bind';
+
+import marked from 'marked';
+
+const cx = classNames.bind(styles);
+
+class MarkdownRender extends Component {
+
+}
+
+export default MarkdownRender;
--- blog-frontend/src/components/editor/EditorPane/EditorPane.js
+++ blog-frontend/src/components/editor/EditorPane/EditorPane.js
@@ -12,7 +12,7 @@ import 'codemirror/mode/shell/shell';
 
 //CodeMirror를 위한 CSS 스타일
 import 'codemirror/lib/codemirror.css';
-import 'codemirror/theme/monkai.css';
+import 'codemirror/theme/monokai.css';
 
 
 const cx = classNames.bind(styles);
@@ -20,28 +20,72 @@ const cx = classNames.bind(styles);
 class EditorPane extends Component {
 	editor = null // 에디터 ref
 	condeMirror = null // CodeMirror 인스턴스
+	cursor = null // 에디터의 텍스트 cursor 위치
 
 	initializeEditor = () => {
 		this.codeMirror = CodeMirror(this.editor, {
 			mode: 'markdown',
-			theme: 'monkai',
+			theme: 'monokai',
 			lineNumbers: true,
 			lineWrapping: true
-		})
+		});
+		this.codeMirror.on('change', this.handleChangeMarkdown);
 	}
 
 	componentDidMount() {
 		this.initializeEditor();
 	}
 
+	handleChange = (e) => {
+		const { onChangeInput } = this.props;
+		const { value, name } = e.target;
+		onChangeInput({name, value});
+	}
+
+	handleChangeMarkdown = (doc) => {
+		const { onChangeInput } = this.props;
+		this.cursor = doc.getCursor();
+		onChangeInput({
+			name: 'markdown',
+			value: doc.getValue()
+		});
+	}
+
+	componentDidUpdate(prevProps, prevState) {
+		// markdown이 변경되면 에디터 값도 변경합니다.
+		// 이 과정에서 택스트 커서의 위치가 초기화되기 때문에,
+		// 저장한 커서의 위치가 있으면 해당 위치로 돌아갑니다.
+		if(prevProps.markdown !== this.props.markdown) {
+			const { codeMirror, cursor } = this;
+			if(!codeMirror) return; // 인스턴스를 아직 만들지 않았을 때
+			codeMirror.setValue(this.props.markdown);
+			if(!cursor) return; // 커서가 없을 때
+			codeMirror.setCursor(cursor); 
+		}
+	}
+
 	render() {
+		const { handleChange } = this;
+		const { tags, title } = this.props; 
+		
 		return (
 			<div className={cx('editor-pane')}>
-				<input className={cx('title')} placeholder="제목을 입력하세요" name="title"/>
+				<input 
+					className={cx('title')} 
+					placeholder="제목을 입력하세요" 
+					name="title"
+					value={title}
+					onChange={handleChange}
+				/>
 				<div className={cx('code-editor')} ref={ref => this.editor=ref}></div>
 				<div className={cx('tags')}>
 					<div className={cx('description')}>태그</div>
-					<input name="tags" placeholder="태그를 입력하세요 (쉼표로 구분)"/>
+					<input 
+						name="tags" 
+						placeholder="태그를 입력하세요 (쉼표로 구분)"
+						value={tags}
+						onChange={handleChange}
+					/>
 				</div>
 			</div>
 		)
