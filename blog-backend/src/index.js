8ffe1d6a270bf30835461c04959cf688c50bece7
kyle_koh <ykoh728@gmail.com>
2018-10-26 15:39:41 +0900

    add login API to backend

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -2,14 +2,16 @@ require('dotenv').config();
 
 const Koa = require('koa');
 const Router = require('koa-router');
-const bodyParser = require('koa-bodyparser')
+const bodyParser = require('koa-bodyparser');
+const session = require('koa-session');
 
 const api = require('./api');
 const mongoose = require('mongoose');
 
 const {
 	PORT: port = 4000,
-	MONGO_URI: mongoURI
+	MONGO_URI: mongoURI,
+	COOKIE_SIGN_KEY: signKey
 } = process.env;
 
 mongoose.Promise = global.Promise;
@@ -25,7 +27,17 @@ const router = new Router();
 //라우터 설정
 router.use('/api', api.routes());
 
+// 라우터 적용 전에 먼저 bodyParser 적용
 app.use(bodyParser());
+
+// 세션/키 적용
+const sessionConfig = {
+	maxAge: 86400000, // 하루
+	// signed: true(기본으로 설정되어 있습니다)
+};
+
+app.use(session(sessionConfig, app));
+app.keys = [signKey];
 
 // app 인스턴스에 라우터 적용
 app.use(router.routes()).use(router.allowedMethods());
